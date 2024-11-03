33236ec7c901de44c553235ea5e4294ad6a662df
kreker41k <watawatqq123@gmail.com>
2024-11-03 04:14:27 +0300

    еще раз выровнил кнопки + сделал темную и светлую темы

--- static/js/profile.js
+++ static/js/profile.js
@@ -10,6 +10,7 @@ function Open(name, eman){
 }
 
 const color=['red', 'orange', 'yellow', 'green', 'lightblue', "blue", "purple", "pink", "broun", "white", "grey", "black"];
+const dtcolors=["#000", "#222", "#fff", "#444", "rgba(255, 72, 72, 0.493)"];
 const elem1 = document.querySelector(".BG_clr");
 const elem2 = document.querySelector(".F_clr");
 const elem3 = document.querySelector(".Text_clr");
@@ -27,4 +28,30 @@ function UpdateTheme(){
     var index3 = Number(elem3.value);
     style.setProperty('--button-color', color[index3]);
 
+}
+
+function LightTheme(){
+    var style = document.documentElement.style;
+    style.setProperty('--bg-color', color[9]);
+    var style = document.documentElement.style;
+    style.setProperty('--bg-color-alt', color[4]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-color', color[5]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-bg', color[4]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-bg-hover', color[5]);
+}
+
+function DarkTheme(){
+    var style = document.documentElement.style;
+    style.setProperty('--bg-color', dtcolors[0]);
+    var style = document.documentElement.style;
+    style.setProperty('--bg-color-alt', dtcolors[1]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-color', dtcolors[2]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-bg', dtcolors[3]);
+    var style = document.documentElement.style;
+    style.setProperty('--button-bg-hover', dtcolors[4]);
 }
