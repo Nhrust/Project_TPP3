9b59c9c7b2d335ffecc97c27481faa26fa20ae67
kreker41k <watawatqq123@gmail.com>
2024-11-11 14:19:49 -0800

    новые темы cсделаны не все

--- static/js/profile.js
+++ static/js/profile.js
@@ -43,17 +43,11 @@ const props_names = [
 ];
 
 const themes = {
-    "Light": ["white", "lightblue", "black", "grey", "#7e7e7e", "#2e2e2e", "#444444", "#777777", "yellow", "orange", "#7c7c7c", "#84dadd", "#bbc3c4", "#d0e3e6", "#ffffff", "#3fe5ff"],
-    "New": [
-        "#0f0f0f", "#1f1f1f",
-        "#ffffff", "#ffefdf",
-        "#ffdfbf", "#000000",
-        "#2f2f2f", "#ffdfbf",
-        "#000000", "#000000",
-        "#000000", "#000000",
-        "#000000", "#000000",
-        "#000000", "#000000",
-    ],
+    "Light": ["white", "lightblue", "black", "grey", "black", "#2e2e2e", "#444444", "#777777", "yellow", "orange", "#7c7c7c", "#84dadd", "#bbc3c4", "#d0e3e6", "#ffffff", "#3fe5ff"],
+    "Dark": ["black", "rgb(56, 56, 56)", "lightred", "darkred", "white", "#7e7e7e", "rgb(68,68,68)", "rgb(255,72,72)", "rgb(55,232,255)", "rgb(176,58,255)", "rgb(255,102,102)", "rgb(155,50,50)", "rgb(201, 32, 32)", "rgb(132, 22, 22)"],
+    "Monochrome": ["Black", "rgb(189, 189, 189)", "rgb(209, 209, 209)", "rgb(122,122,122)", "black", "rgb(194,194,194", "rgb(163, 163, 163)", "rgb(94,94,94)", "rgb(145, 173, 186)", "rgb(143, 116, 170)", "rgb(178, 149, 149)", "rgb(181, 120, 120", "white", "white"],
+    "Mint": ["#22d889", "#369b67", "#ffffff", "#ddddff", "#ffffff", "#ddddff", "#3ed08c", "#ff4848", "#45ff38", "#a5ff38", "#57c185", "#329a64", "#29d19e", "#168886"],
+    "RedAndBlack": ["#000000", "#1c1c1c", "#ff0000", "#700000", "#ff0000", "#700000", "#000000", "#000000", "#c15757", "#612929", "#b83d3d", "#6d5454", "#ff0000", "#000000"],
 }
 
 function UpdateTheme() {
